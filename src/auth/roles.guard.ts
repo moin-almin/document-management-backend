c094a4adab170a5c7b1b6d47c6b036bc82def936
Moin Almin <almin.crypt@gmail.com>
2024-12-14 17:16:26 +0530

    chore: debugger points added

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -7,14 +7,19 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.get<RoleEnum[]>(
-      'roles',
-      context.getHandler(),
-    );
-    if (!requiredRoles) {
-      return true;
-    }
+    const requiredRoles = this.reflector.get<RoleEnum[]>('roles', context.getHandler());
+    console.log('Required Roles:', requiredRoles); // Debug required roles
+
     const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user?.role?.name === role);
+    console.log('User Info:', user); // Debug user from request
+
+    if (!requiredRoles) {
+      return true; // If no roles are required, grant access
+    }
+
+    const hasRole = requiredRoles.some((role) => user?.role === role);
+    console.log('Has Required Role:', hasRole); // Debug role matching result
+
+    return hasRole;
   }
 }
