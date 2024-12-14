24942570543449009f46df5fdc6e5d053b01150b
Moin Almin <almin.crypt@gmail.com>
2024-12-14 23:34:23 +0530

    fix: register document entity

--- src/document/document.module.ts
+++ src/document/document.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Document } from './document.entity';
 import { DocumentService } from './document.service';
 import { DocumentController } from './document.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Document])], // Register the Document entity
   providers: [DocumentService],
-  controllers: [DocumentController]
+  controllers: [DocumentController],
 })
 export class DocumentModule {}
